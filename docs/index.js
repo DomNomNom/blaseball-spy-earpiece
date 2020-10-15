2650b3e167fb56ae20393a961fc4f12d5de11af3
Dominik Schmid <dominikschmid93@gmail.com>
2020-10-16 01:19:50 +1100

    Implement all the things.

--- docs/index.js
+++ docs/index.js
@@ -3,17 +3,57 @@ const get = id => document.getElementById(id);
 
 function main() {
     get('loading').classList.add('hidden')
-    get('encrypt').classList.remove('hidden')
-    get('encrypt-message').onchange = updateEncryptOutput;
-    get('encrypt-message').onkeyup = updateEncryptOutput;
+    get('encrypt-message').onchange  = updateEncryptOutput;
+    get('encrypt-message').onkeyup   = updateEncryptOutput;
     get('encrypt-password').onchange = updateEncryptOutput;
+    get('encrypt-password').onkeyup  = updateEncryptOutput;
     get('encrypt-output-copy').onclick = copyLink;
+    get('decrypt-password').onchange = updateDecryptOutput;
+    get('decrypt-password').onkeyup = hidePasswordError;
 
-    // if ()
+    const url = new URL(document.location);
+    const cyphertext = url.searchParams.get('m');
+    const want_hash = url.searchParams.get('h');
+    if (cyphertext) {
+        get('decrypt').classList.remove('hidden')
+        get('decrypt-cyphertext').value = cyphertext;
+        get('decrypt-pwhash').value = want_hash;
+    } else {
+        get('encrypt').classList.remove('hidden')
+    }
     // get('encrypt-message').value = 'updateEncryptOutput';
     // get('encrypt-password').value = 'updateEncryptOutput';
     // updateEncryptOutput();
 }
+
+
+function updateDecryptOutput() {
+    const password = get('decrypt-password').value;
+    const want_hash = get('decrypt-pwhash').value;
+    if (want_hash && want_hash != pwhash(password)) {
+        if (password.toLowerCase() == 'password') {
+            document.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; // HAHA
+        }
+        get('decrypt-wrong-password').classList.remove('hidden');
+        get('decrypt-plaintext').classList.add('hidden')
+        return
+    }
+    hidePasswordError();
+    const plain_textarea = get('decrypt-plaintext');
+    plain_textarea.classList.remove('hidden');
+    const plaintext = CryptoJS.AES.decrypt(get('decrypt-cyphertext').value, password).toString(CryptoJS.enc.Utf8);
+    plain_textarea.textContent = plaintext;
+}
+
+function hidePasswordError(e) {
+    if (e && e.key == "Enter") return
+    get('decrypt-wrong-password').classList.add('hidden');
+}
+
+//
+// Encrypt
+//
+
 
 function copyLink(e) {
     e.preventDefault();
@@ -44,7 +84,7 @@ function updateEncryptOutput() {
     url.hash = ''
     link.textContent = url;
     link.href = url;
-    console.log('hi', message)
+    get('encrypt-output-copy').textContent = `Copy`;
 }
 
 const salt = 'spies win';
